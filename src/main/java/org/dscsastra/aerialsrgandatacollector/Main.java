fd202ce5776a2470543c0decfcd8ff8fce6d297c
Vishal R <vishal27.rs@outlook.com>
2019-08-20 11:13:40 +0530

    Add gradle and Main files

---
+++ src/main/java/org/dscsastra/aerialsrgandatacollector/Main.java
@@ -0,0 +1,7 @@
+package org.dscsastra.aerialsrgandatacollector;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/org/dscsastra/aerialsrgandatacollector/Test1.java
@@ -0,0 +1,11 @@
+package org.dscsastra.aerialsrgandatacollector;
+
+import org.junit.Test;
+
+public class Test1 {
+
+    @Test
+    void test(){
+
+    }
+}
